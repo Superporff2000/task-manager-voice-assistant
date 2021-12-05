feee1777f5d61b45bc006350c1e84842a3645fa8
Super Porff <Superporff2000@gmail.com>
2021-12-05 20:39:01 +0530

    Implemented the functionality for Setting Process Priority. Not sure if it is good and robust or not, but it gets the job done good enough. Now to work on creating dump files of processes and showing them if necessary in Visual Studio.

--- script.py
+++ script.py
@@ -1,13 +1,39 @@
 from os import system
 
-# Implementation of End task (GUI-less)
-
+# End task (GUI-less, voice-input-less)
 def endTask(processName):
     killProcess = 'taskkill /im {} /F'.format(processName)  # Statement to be executed
-    
-    try:
-        os.system(killProcess)
-    except PermissionError as pe:
-        print(pe, ' :The task could not be ended due to Permission Error')
 
-endTask('taskmgr.exe')  #Test function call to close Task Manager window if it is open
+    try:
+        system(killProcess)
+    except OSError as e:
+        print(e, ' :The task could not be ended due to Permission Error')
+
+#  Set Priority (GUI-less, Voice-input-less)
+def setPriority(value):
+    from psutil import Process
+
+    priorityStatus = {
+        64 : 'Low',
+        16384 : 'Below Normal',
+        32:'Normal',
+        32768: 'Above Normal',
+        128 : 'High'
+    }
+
+    '''Set the Process Priority'''
+    p = Process()
+
+    if value in priorityStatus:
+        p.nice(value)
+
+    # Console Output statement for confirmation
+        for i in priorityStatus:
+            if i == value:
+                print( 'The current program\'s priority has been set to {0}'.format(priorityStatus[value]) )
+    else:
+        print('The given value is not valid')
+
+setPriority(32)
+
+# Create debug log and store it
